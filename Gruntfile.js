ac330d5aeb467b02cd179391a5e2b179c3acc95e
Tom Randolph <trandolph@five-startech.com>
2015-01-22 22:53:18 -0600

    Update Gruntfile, images directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt){
                 "files": [
                     {
                         "expand": true,
-                        "cwd":  'src/img/',
+                        "cwd":  'src/images/',
                         "src": ['**'],
                         "dest": 'build/img/'
                     }
